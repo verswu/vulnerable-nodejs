d3f81ffbe17800f5aaa152ee733335821a39a5a5
verswu <wubird@gmail.com>
2022-09-22 23:19:40 -0700

    hello world express server

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8080;
+
+//App
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Running on ${PORT}`);
+});
+
