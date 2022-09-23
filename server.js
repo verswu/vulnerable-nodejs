b0bee09e92f82379ff3a3063616cb213db739e4e
verswu <wubird@gmail.com>
2022-09-23 15:08:38 -0700

    added js scripts

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const PORT = 8080;
 
 //App
 const app = express();
+app.use(favicon(__dirname + '/assets/favicon.ico'));
+app.use("/static", express.static('./static/'));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/html/index.html'));
+    res.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
 app.listen(PORT, () => {
