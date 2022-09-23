a8d0c0c2f833e960de4db51705b0e0a953d97beb
verswu <wubird@gmail.com>
2022-09-22 23:39:19 -0700

    add html

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 const PORT = 8080;
 
 //App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, '/html/index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Running on ${PORT}`);
+    console.log(`Running on http://localhost:${PORT}/`);
 });
 
